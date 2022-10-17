93371a3d7d49ce0d623b272c0552c65500ca094b
lisa271 <lizbmaloba@gmail.com>
2022-10-16 18:41:12 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print numerbz
+ * Return: o
+ */
+int main(void)
+{
+	int a;
+	for (a = 48; a < 58; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
+
