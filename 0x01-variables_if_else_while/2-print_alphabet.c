2f1e66dcb65022f8fac942f76cd155c0da571019
lisa271 <lizbmaloba@gmail.com>
2022-10-16 17:06:16 -0700

    lowercase alphabets

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,13 +3,14 @@
  * main - print lowercase alphabet
  * Return: 0
  */
-int main (void)
+int main(void)
 {
 	char letter;
+
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 	putchar(letter);
 	}
 	putchar('\n');
-	return 0;
+	return (0);
 }
