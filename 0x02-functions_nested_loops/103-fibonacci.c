a6e5035c1e4e39c8d290364508e42d33f5cc2024
lisa271 <lizbmaloba@gmail.com>
2022-10-19 14:15:06 -0700

    fibonnaci even numbers

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - prints sum of even valued_numbers
+ * followed y new line
+ */
+int main(void)
+{
+	unsigned long fibo1 = 0, fibo2 = 1, fibosum;
+	float total_sum;
+
+	while (1)
+	{
+		fibosum = fibo1 + fibo2;
+		if (fibosum > 4000000)
+			break;
+		if ((fibosum % 2) == 0)
+			tot_sum += fibosum;
+		fibo1 = fibo2;
+		fibo2 = fibosum;
+	}
+	printf("%.0f\n", total_sum);
+	return (0);
+}
