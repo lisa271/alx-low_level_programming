bd868059faae2c87e66e508c2f94695e190138ef
lisa271 <lizbmaloba@gmail.com>
2022-10-21 18:44:18 -0700

    more numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,14 +3,13 @@
 *print_numbers - print 0123456789
 *Return: void
 */
-
 void print_numbers(void)
 {
 	char c;
 
 	for (c = '0'; c <= '9'; c++)
 	{
-	_putchar(c);
+		_putchar(c);
 	}
 	_putchar('\n');
 }
---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,5 @@
+#ifndef MAIN
+#define MAIN
+void print_numbers(void);
+int _putchar(int c);
+#endif
