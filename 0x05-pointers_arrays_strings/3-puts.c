cdc79b9f21bc4e64f8c8807c47bafb59bae0e108
lisa271 <lizbmaloba@gmail.com>
2022-10-25 15:21:52 -0700

    puts string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts - prints a string
+ * @str: enter string
+ * Return: string length
+ */
+void _puts(char *str)
+{
+	int l = 0;
+
+	while (str[l] != '\0')
+	{
+		l++;
+		s+=;
+	}
+	return(l);
+}
