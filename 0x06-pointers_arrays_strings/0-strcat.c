7579a682b6ea09280da1ee041f72f1938210a8e5
lisa271 <lizbmaloba@gmail.com>
2022-10-28 09:35:21 -0700

    concatenate two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -17,10 +17,10 @@ char *_strcat(char *dest, char *src);
 	}
 	j = 0;
 	while (src([j] != '\0')
-	{
-			dest[i] = src[j];
-			i++;
-			j++;
+ 	{
+		dest[i] = src[j];
+		i++;
+		j++;
 	}
 	dest[i] = '\0';
 	return (dest);
