04abd327b8ff6e1887716b77a927837b3940755b
lisa271 <lizbmaloba@gmail.com>
2022-11-15 14:23:08 -0800

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dogs basic information
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ * Description: longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+/**
+ * dog_t -typedef for struct dog
+ */
+
